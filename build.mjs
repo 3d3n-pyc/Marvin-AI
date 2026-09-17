fbc65827e354ffcc390d5ff57aa4a10bb9760bfd
Maxime Lefevre <maxime1.lefevre@epitech.eu>
2026-09-17 02:25:14 +0200

    fix(firefox): add mandatory data_collection_permissions to manifest

--- build.mjs
+++ build.mjs
@@ -127,7 +127,10 @@ function getFirefoxManifest() {
     browser_specific_settings: {
       gecko: {
         id: 'marvin-unlock@myepitech',
-        strict_min_version: '109.0'
+        strict_min_version: '109.0',
+        data_collection_permissions: {
+          required: ['none']
+        }
       }
     }
   };
